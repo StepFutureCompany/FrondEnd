4ef1ce502c54dd48cd329215cd8104df08f75cf8
Pedro-Clear <pedro.xavier.clear@gmail.com>
2023-11-28 16:04:02 -0300

    fix: Review Changes

--- src/components/DadosDoFuncionarios/index.js
+++ src/components/DadosDoFuncionarios/index.js
@@ -2,52 +2,161 @@ import React, { useState } from "react";
 import Header from "../Header";
 import InputText from "../InputText";
 import Modal from "../Modal";
+import api_client from "../../config/api_client";
 
 export default function DadosDoFuncionarios() {
   const [renderModal, setRenderModal] = useState(false);
+  const [stateBairro, setStateBairro] = useState('');
+  const [stateCargo, setStateCargo] = useState('');
+  const [stateCelular, setStateCelular] = useState('');
+  const [stateCep, setStateCep] = useState('');
+  const [stateCidade, setStateCidade] = useState('');
+  const [stateRua, setStateRua] = useState('');
+  const [stateNumeroCasa, setStateNumeroCasa] = useState('');
+  const [stateCpf, setStateCpf] = useState('');
+  const [stateRg, setStateRg] = useState('');
+  const [stateCtTrabalho, setStateCtTrabalho] = useState('');
+  const [statePis, setStatePis] = useState('');
+  const [stateNascimento, setStateNascimento] = useState('');
+  const [stateEmail, setStateEmail] = useState('');
+  const [stateSalarioLiquido, setStateSalarioLiquido] = useState('');
+  const [stateRa, setStateRa] = useState('');
+
+  function handleSubmit() {
+    api_client
+      .post("/funcionario", {
+        ra: stateRa,
+        cpf: stateCpf,
+        salarioLiquido: stateSalarioLiquido,
+        dtNascimento: stateNascimento,
+        rg: stateRg,
+        ctTrabalho: stateCtTrabalho,
+        cargo: stateCargo,
+        email: stateEmail,
+        pis: statePis,
+        rua: stateRua,
+        numero: stateNumeroCasa,
+        bairro: stateBairro,
+        cep: stateCep,
+        cidade: stateCidade,
+        celular: stateCelular,
+      })
+      .then((response) => {
+        console.log("Sucesso", response);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
 
   return (
     <>
-      <Modal title="Holerite" show={renderModal} close={setRenderModal} inputs/>
+      <Modal
+        title="Holerite"
+        show={renderModal}
+        close={setRenderModal}
+        inputs
+      />
       <section className="w-screen h-screen flex bg-blue-500 flex-col items-center text-3xl">
         <Header />
         <div className="w-[70%] h-full flex flex-col gap-3 justify-center items-center rounded-xl p-5 bg-white m-5">
           <h1 className="text-2xl font-semibold">Dados Do Funcionarios</h1>
           <article className="flex w-full gap-10 text-start">
             <aside className="w-full flex flex-col gap-2">
-              <InputText title="RA" />
-              <InputText title="Data de nascimento" />
-              <InputText title="Cargo" />
+              <InputText
+                title="RA"
+                value={stateRa}
+                onChange={(e) => setStateRa(e.target.value)}
+              />
+              <InputText
+                title="Data de nascimento"
+                value={stateNascimento}
+                onChange={(e) => setStateNascimento(e.target.value)}
+              />
+              <InputText
+                title="Cargo"
+                value={stateCargo}
+                onChange={(e) => setStateCargo(e.target.value)}
+              />
             </aside>
             <aside className="w-full flex flex-col gap-2">
-              <InputText title="CPF" />
-              <InputText title="RG" />
-              <InputText title="Email" />
+              <InputText
+                title="CPF"
+                value={stateCpf}
+                onChange={(e) => setStateCpf(e.target.value)}
+              />
+              <InputText
+                title="RG"
+                value={stateRg}
+                onChange={(e) => setStateRg(e.target.value)}
+              />
+              <InputText
+                title="Email"
+                value={stateEmail}
+                onChange={(e) => setStateEmail(e.target.value)}
+              />
             </aside>
             <aside className="w-full flex flex-col gap-2">
-              <InputText title="Salario Liquido" />
-              <InputText title="Carteira de Trabalho" />
-              <InputText title="PIS" />
+              <InputText
+                title="Salario Liquido"
+                value={stateSalarioLiquido}
+                onChange={(e) => setStateSalarioLiquido(e.target.value)}
+              />
+
+              <InputText
+                title="Carteira de Trabalho"
+                value={stateCtTrabalho}
+                onChange={(e) => setStateCtTrabalho(e.target.value)}
+              />
+              <InputText
+                title="PIS"
+                value={statePis}
+                onChange={(e) => setStatePis(e.target.value)}
+              />
             </aside>
           </article>
           <h1 className="text-2xl font-semibold">Endereço</h1>
           <article className="flex w-full gap-10 text-start">
             <aside className="w-full flex flex-col gap-2">
-              <InputText title="Rua" />
-              <InputText title="Bairro" />
-              <InputText title="Cidade" />
+              <InputText
+                title="Rua"
+                value={stateRua}
+                onChange={(e) => setStateRua(e.target.value)}
+              />
+              <InputText
+                title="Bairro"
+                value={stateBairro}
+                onChange={(e) => setStateBairro(e.target.value)}
+              />
+              <InputText
+                title="Cidade"
+                value={stateCidade}
+                onChange={(e) => setStateCidade(e.target.value)}
+              />
             </aside>
             <aside className="w-full flex flex-col gap-2">
-              <InputText title="Numero" />
-              <InputText title="CEP" />
-              <InputText title="Celular" />
+              <InputText
+                title="Numero"
+                value={stateNumeroCasa}
+                onChange={(e) => setStateNumeroCasa(e.target.value)}
+              />
+              <InputText
+                title="CEP"
+                value={stateCep}
+                onChange={(e) => setStateCep(e.target.value)}
+              />
+              <InputText
+                title="Celular"
+                value={stateCelular}
+                onChange={(e) => setStateCelular(e.target.value)}
+              />
             </aside>
           </article>
           <button
             className="text-lg w-[400px] h-10 rounded-xl bg-blue-500 text-white"
-            onClick={() => setRenderModal(true)}
+            onClick={() => handleSubmit()}
           >
-            Holerite
+            Salvar
           </button>
         </div>
       </section>
